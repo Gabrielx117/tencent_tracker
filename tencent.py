f38b4592b7c9667501ab9c84d0201f29d794cbca
Gabrielx117 <xiayu@btte.net>
2017-06-24 12:52:54 +0800

    修改为python3 版本
    1.修改压平list所用方法，原方法isinstance不在生效
    2.filter返回值变更，需要list化
    3.urllib模块拆分，现在改为urllib.request
    4.解决history读取失败问题，读取失败默认为空
    5.简化差值计算代码

--- tencent.py
+++ tencent.py
@@ -1,74 +1,102 @@
-#!/usr/bin/env python
-#coding=utf-8
-import time,urllib,sys
-import json,re,pickle,smtplib
+#!/usr/bin/env python3
+# coding=utf-8
+import time
+import urllib.request
+import sys
+import json
+import re
+import pickle
+import smtplib
 from collections import Iterable
 from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 
-DEBUG=0
-tencent_tracker='%s/tencent.last.json' %sys.path[0]
-history='%s/history.json' %sys.path[0]
-email='%s/email.json' %sys.path[0]
-with open(email, 'r') as f:
-    email_info = json.load(f)
+DEBUG = 0
+tencent_tracker = '%s/tencent.tracker.json' % sys.path[0]
+history = '%s/history.json' % sys.path[0]
+email = '%s/email.json' % sys.path[0]
 
-to_addr=email_info['to_addr']
+with open(email, 'r', encoding='utf-8') as f:
+    email_info = json.load(f)
+to_addr = email_info.get('to_addr')
 from_addr = email_info['from_addr']
 password = email_info['passwd']
 smtp_server = email_info['smtp_server']
-email_list=[]
-date=time.strftime("%Y%m%d", time.localtime())
-url='http://play.domain.qq.com/getdomain.php?dtime=%s' %date
 
-urllib.urlretrieve(url,tencent_tracker)
+date = time.strftime("%Y%m%d", time.localtime())
+url = 'http://play.domain.qq.com/getdomain.php?dtime=%s' % date
 
-def is_ipv4(data): #"IPv4过滤函数"
-    ipv4 = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$') #ipv4正则表达式
+#urllib.request.urlretrieve(url, tencent_tracker)
+
+
+def is_ipv4(data):  # "IPv4过滤函数"
+    ipv4 = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')  # ipv4正则表达式
     return ipv4.match(data)
+
+
 def _format_addr(s):
     name, addr = parseaddr(s)
-    return formataddr((Header(name, 'utf-8').encode(), addr))
+    return formataddr((Header(name).encode(), addr))
 
 
-
-with open(tencent_tracker, 'r') as f:
+with open(tencent_tracker, 'r') as f:  # 读取新文件
     tracker = json.load(f)
 
-with open(history, 'rb') as i: #获取上一次列表
-    old = set(pickle.load(i))
+# 获取上一次列表
+try:
+    with open(history, 'rb') as i:
+        old = set(pickle.load(i))
+except:
+    IndexError
+    old = set()
 
-flat=lambda L: sum(map(flat,L),[]) if isinstance(L,list) else [L] #"压平列表"
-l=flat(tracker['data'].values())
 
-ip=filter(is_ipv4,l) #过滤ipv4
-new=set(ip) #生成新列表
+# 生成新列表
+flattened = [val for sublist in tracker['data'].values()
+             for val in sublist]  # "压平列表"
+ip = list(filter(is_ipv4, flattened))  # 过滤ipv4
+new = set(ip)
 
-with open(history, 'wb') as i: #将本次列表写入历史文件
-    pickle.dump(ip,i)
 
-add=str('\n'.join(sorted(list((new.difference(old)))))) or '暂无' #新增差量
-remove=str('\n'.join(sorted(list(old.difference(new))))) or '暂无' #删除差量
+with open(history, 'wb') as i:  # 将本次列表写入历史文件
+    pickle.dump(ip, i)
 
+
+add = '\n'.join(sorted(new - old)) or '暂无'  # 新增差量
+remove = '\n'.join(sorted(old - new)) or '暂无'  # 删除差量
+
+
+email_list = []  # 生成收件人列表
 for k in to_addr.keys():
-    email_list.append(_format_addr('%s <%s>' %(k,to_addr[k])))
-email_list=','.join(email_list)
-msg = MIMEText('新增\n%s\n删除\n%s' % (add,remove), 'plain', 'utf-8')
+    email_list.append(_format_addr('%s <%s>' % (k, to_addr[k])))
+email_list = ','.join(email_list)
+
+
+# 填写邮件内容
+msg = MIMEText('新增\n%s\n删除\n%s' % (add, remove), 'plain', 'utf-8')
 msg['From'] = _format_addr('天津网管监控 <%s>' % str(from_addr))
 msg['To'] = email_list
-msg['Subject'] = Header('腾讯Tracker更新(%s)'%str(tracker['atime']), 'utf-8').encode()
+msg['Subject'] = Header('腾讯Tracker更新(%s)' %
+                        str(tracker['atime'])).encode()
 
+
+# 发送邮件
 server = smtplib.SMTP(smtp_server, 25)
-if DEBUG == 1:
-    server.set_debuglevel(1)
-    print (date)
-    print (url)
-    print('add')
-    print (add)
-    print ('remove')
-    print (str(remove))
 server.login(from_addr, password)
 server.sendmail(from_addr, to_addr.values(), msg.as_string())
 server.quit()
+
+if DEBUG == 1:
+    import io
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    server.set_debuglevel(1)
+    # print(date)
+    # print(url)
+    # print(new)
+    # print(old)
+    print('add')
+    print(add)
+    print('remove')
+    print(remove)
